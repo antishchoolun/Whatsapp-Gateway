753152caa5dfcf6ab172e327f9172a96aa7f989e
Jayesh Choolun <antishchoolun95@gmail.com>
2022-08-10 17:27:01 +0400

    add

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: node server.js
-web: vendor/bin/heroku-php-apache2 public/
+web: node server.js & vendor/bin/heroku-php-apache2 public/
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app)
  */
 const {Server} = require('socket.io');
 const io = new Server(server)
-const port = process.env.PORT || 3000
+const port = process.env.PORT_NODE
 // const io = require('socket.io')(server, {
 //     cors: {
 //         origin: process.env.ORIGIN
